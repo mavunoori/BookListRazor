ad45fdaea47b69a477da9cf441de3d60589ead94
avunoma <33365065+avunoma@users.noreply.github.com>
2020-06-07 00:31:13 +0530

    Add project files.

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await db.Book.AddAsync(Book);
+                await db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public IndexModel(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public List<Book> Books { get; private set; }
+
+        public async Task OnGet()
+        {
+            Books = await _dbContext.Book.ToListAsync<Book>();
+        }
+
+        public async Task<ActionResult> OnPostDelete(int id)
+        {
+            var book = await _dbContext.Book.FindAsync(id);
+            if(book ==null)
+            {
+                return NotFound();
+            }
+            _dbContext.Book.Remove(book);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("Index");
+
+        }
+
+    }
+}
